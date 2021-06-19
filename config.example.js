1b0db8df11fb7668fcb79b76c499438922af709d
Pulipuli Chen <blog@pulipuli.info>
2021-06-20 01:35:14 +0800

    0620 著

--- config.example.js
+++ config.example.js
@@ -2,6 +2,7 @@ module.exports = {
   dictPath: 'C:\\Users\\USER\\AppData\\Roaming\\Rime\\terra_pinyin.mine.dict.yaml',
   backupDictPath: null,
   deployCommand: '"C:\\Program Files (x86)\\Rime\\weasel-0.14.3\\WeaselDeployer.exe" /deploy',
+  //deployCommand: 'sh /home/user/Documents/NetBeansProjects/[electron]/RIME-User-Dict-Editor/bin/rime-redeploy.sh',
   debug: {
     openDevTools: false
   }
