22c6a348b4f7ed367419144e7080bbfd249c25fe
Pulipuli Chen <pulipuli.chen@gmail.com>
2021-12-31 22:21:54 +0800

    1231 Windows安裝的設定，整理完成

--- config.example.js
+++ config.example.js
@@ -1,5 +1,7 @@
 module.exports = {
   dictPath: 'C:\\Users\\USER\\AppData\\Roaming\\Rime\\terra_pinyin.mine.dict.yaml',
+  // dictPath: 'C:\\Users\\USER\\AppData\\Roaming\\Rime\\build\\terra_pinyin.mine.dict.yaml',
+  // 其他說明：https://docs.google.com/document/d/1dNHNo6hi4vsU2Kg9S3lC7LDj1_iX78cUIw4e2CzrHls/edit
   backupDictPath: null,
   deployCommand: '"C:\\Program Files (x86)\\Rime\\weasel-0.14.3\\WeaselDeployer.exe" /deploy',
   //deployCommand: 'sh /home/user/Documents/NetBeansProjects/[electron]/RIME-User-Dict-Editor/bin/rime-redeploy.sh',
